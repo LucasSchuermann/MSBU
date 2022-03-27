25f654ba62535b6fb2e1f74db0a755cb00efd65b
LucasSchuermann <lux32@gmx.de>
2022-03-27 20:31:41 +0200

    fixed Projectstructure

---
+++ Assets/Skripts/SteuerungTyp.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteuerungTyp : MonoBehaviour
+{
+   
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    
+    private bool jumpKeyPressed;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {  
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * 5, rigidbodyComponent.velocity.y, verticalInput * 5);
+        
+        if (Physics.OverlapSphere(groundCheckTransform.position,.1f, playerMask).Length == 0) 
+        { 
+           return;
+        }
+        if (jumpKeyPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyPressed = false;
+        }
+       
+    }
+
+   
+}
